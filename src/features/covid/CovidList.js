790db1097ec275cbc7c68e5b4695dd6332e4aa10
MucaBordini <mucabordini@gmail.com>
2020-08-25 13:26:53 -0300

    Acrescentado o axios pegando os dados da API

---
+++ src/features/covid/CovidList.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function CovidList() {
+
+  const [casos , setCasos] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(
+        'https://covid19-brazil-api.now.sh/api/report/v1'
+      );
+      setCasos(res.data.data);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>
+        {casos.map((caso, index) => {
+          return <li key={index}>Estado: {caso.uf} Casos: {caso.cases}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default CovidList;

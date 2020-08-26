5c140d7569bb2c22cff6b993017c8590034bf849
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-08-25 22:41:33 -0300

    Adicionado funcionalidade de busca por estado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CovidList from './features/covid/CovidList'
+
 function App() {
   return (
     <div className="App">
--- src/features/covid/CovidList.js
+++ src/features/covid/CovidList.js
@@ -2,25 +2,43 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function CovidList() {
-
-  const [casos , setCasos] = useState([]);
+  const [casos, setCasos] = useState([]);
+  const [uf, setUf] = useState('');
 
   useEffect(() => {
     (async () => {
       const res = await axios.get(
         'https://covid19-brazil-api.now.sh/api/report/v1'
       );
+
       setCasos(res.data.data);
     })();
   }, []);
 
+  async function search() {
+    const res = await axios.get(
+      `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${uf}`
+    );
+
+    setCasos([res.data])
+  }
+
   return (
-    <div className="App">
+    <div className="covid_list">
+
+      <select value={uf} onChange={(ev) => setUf(ev.target.value)}>
+        <option disabled selected value=''>Selecione um estado</option>
+        <option value='sp'>São Paulo</option>
+        <option value='pr'>Paraná</option>
+      </select>
+      <button onClick={() => search()}>Search</button>
+
       <ul>
         {casos.map((caso, index) => {
           return <li key={index}>Estado: {caso.uf} Casos: {caso.cases}</li>;
         })}
       </ul>
+
     </div>
   );
 }

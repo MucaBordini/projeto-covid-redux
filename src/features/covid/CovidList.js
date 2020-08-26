dc1945f91224e3be34dc509c9e47cae4cc7739b1
MucaBordini <mucabordini@gmail.com>
2020-08-26 20:05:10 -0300

    Adicionado os estados na busca

--- src/features/covid/CovidList.js
+++ src/features/covid/CovidList.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { casos_map, find_casos, search_casos } from './covidSlice';
+import { casos_map, find_casos, search_casos, estados_map, find_estados } from './covidSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import './CovidList.css'
 
 function CovidList() {
   const casos = useSelector(casos_map);
+  const estados = useSelector(estados_map);
   const [uf, setUf] = useState('');
   const dispatch = useDispatch();
-
+  
   useEffect(() => {
     dispatch(find_casos());
+    dispatch(find_estados());
   }, [dispatch]);
 
   return (
@@ -17,13 +19,18 @@ function CovidList() {
 
       <select className="selectstate" defaultValue={'DEFAULT'} onChange={(ev) => setUf(ev.target.value)}>
         <option hidden value='DEFAULT'>Selecione um estado</option>
-        <option value='sp'>São Paulo</option>
-        <option value='pr'>Paraná</option>
+        <option value='todos'>Todos os estados</option>
+        {estados.map((estado, index) => {
+              return (
+                <option value={estado.uf} key={index}>{estado.state}</option>
+              );
+            })}
       </select>
       <button className='pesquisa' onClick={() => dispatch(search_casos(uf))}>Search</button>
-        <table class="tabela">
-        <caption class="legendatabela">Covid no Brasil</caption>
-            <thead class="cabecalho">
+
+        <table className="tabela">
+        <caption className="legendatabela">Covid no Brasil</caption>
+            <thead className="cabecalho">
             <tr>
                 <th className='cabecalho' scope="col">Estado</th>
                 <th className='cabecalho' scope="col">Casos</th>
@@ -35,13 +42,13 @@ function CovidList() {
             <tbody>
             {casos.map((caso, index) => {
               return (
-                <tr>
-                <td className='itens' key={index}>{caso.uf}</td>
-                <td className='itens' key={index}>{caso.cases}</td>
-                <td className='itens' key={index}>{caso.deaths}</td>
-                <td className='itens' key={index}>{caso.suspects}</td>
-                <td className='itens' key={index}>{caso.datetime}</td>
-                </tr>
+                <tr key={index}>
+                <td className='itens'>{caso.uf}</td>
+                <td className='itens'>{caso.cases}</td>
+                <td className='itens'>{caso.deaths}</td>
+                <td className='itens'>{caso.suspects}</td>
+                <td className='itens'>{caso.datetime}</td>
+                </tr>            
               );
             })}
             </tbody>
--- src/features/covid/covidSlice.js
+++ src/features/covid/covidSlice.js
@@ -3,17 +3,29 @@ import axios from 'axios';
 
 export const covidSlice = createSlice({
   name: 'covid',
-  initialState: { casos: [] },
+  initialState: { casos: [], estados: [] },
   reducers: {
     set_casos: (state, action) => {
       state.casos = action.payload;
+      return state;
+    },
+    set_estados: (state, action) => {
+      state.estados = action.payload;
       return state;
     }
   }
 });
 
 export const casos_map = (state) => state.covid.casos;
+export const estados_map = (state) => state.covid.estados;
 export const covidActions = covidSlice.actions;
+export const find_estados = () => async (dispatch) => {
+  const res = await axios.get(
+    'https://covid19-brazil-api.now.sh/api/report/v1'
+  );
+
+  dispatch(covidActions.set_estados(res.data.data));
+}
 export const find_casos = () => async (dispatch) => {
   const res = await axios.get(
     'https://covid19-brazil-api.now.sh/api/report/v1'
@@ -22,10 +34,18 @@ export const find_casos = () => async (dispatch) => {
   dispatch(covidActions.set_casos(res.data.data));
 }
 export const search_casos = (uf) => async (dispatch) => {
-  const res = await axios.get(
-    `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${uf}`
-  );
-
-  dispatch(covidActions.set_casos([res.data]));
+  console.log(uf);
+  if (uf !== "todos"){
+    const res = await axios.get(
+      `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${uf}`
+    );
+    
+    dispatch(covidActions.set_casos([res.data]));
+  } else {
+    const res = await axios.get(
+      'https://covid19-brazil-api.now.sh/api/report/v1'
+    );
+    dispatch(covidActions.set_estados(res.data.data));
+  }
 }
 export default covidSlice.reducer;

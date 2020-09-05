77d52ab5ada30eeb61dbeb192a917bd453eb0abc
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-04 22:09:51 -0300

    Deletando component Form e movendo para o Header

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { user_map, login } from './headerSlice';
+import { user_map, login, register } from './headerSlice';
 
 import logo from '../../assets/coronavirus.png'
 import './Header.css';
-import Form from '../formRegister/form';
-
 
 function Header() {
     const user = useSelector(user_map);
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
+    const [password, setPassword] = useState('');
     const [logado] = useState(localStorage.getItem('logged'))
     const dispatch = useDispatch();
 
@@ -28,56 +26,57 @@ function Header() {
         if (localStorage.getItem('token') === 'undefined'){
             localStorage.setItem('token', `${user.token}`)
         } 
-      }, [logado, user.token]);
-
-    
+    }, [logado, user.token]);
 
     return (
-
-        
         <div className='container'>
             <div className='image'>
-                <img src={logo} alt="test"/>
+                <img src={logo} alt="covid logo"/>
             </div>
-            {logado ? (
-            <button onClick={() => {
-                localStorage.clear();
-                window.location.reload(false);
-            }}>Sair</button>
-            )
-            : 
-            (<div className='inputs'>
-                <label>Email: </label>
-                <input className='campoEmail' type='email' onChange={(ev) => setEmail(ev.target.value)}></input>
-                    
-                <label>Senha: </label>
-                <input className='campoSenha' type='password' onChange={(ev) => setPassword(ev.target.value)}></input>
-                    
-                <div>
-                    <button className="botao" onClick={() => {
-                        dispatch(login(email, password));
-                        localStorage.setItem('logged', true);
-                        setTimeout(function() {
-                            window.location.reload(false);
-                        }, 1000);
-                    }}>LOGIN</button>
-                    
-                </div>
-                <div> 
-                    <button className="botao" onClick={() => handleToggleForm()}>REGISTRAR-SE</button>
-                </div>
-            </div>)
+            {
+                logado ? (
+                    <button onClick={() => {
+                        localStorage.clear();
+                        window.location.reload(false);
+                    }}>Sair</button>
+                )
+                :
+                (<div className='inputs'>
+                    <label>Email: </label>
+                    <input className='campoEmail' type='email' onChange={(ev) => setEmail(ev.target.value)}></input>
+                        
+                    <label>Senha: </label>
+                    <input className='campoSenha' type='password' onChange={(ev) => setPassword(ev.target.value)}></input>
+                        
+                    <div>
+                        <button className="botao" onClick={() => {
+                            dispatch(login(email, password));
+                            localStorage.setItem('logged', true);
+                            setTimeout(function() {
+                                window.location.reload(false);
+                            }, 1000);
+                        }}>LOGIN</button>
+                        
+                    </div>
+                    <div> 
+                        <button className="botao" onClick={() => handleToggleForm()}>REGISTRAR-SE</button>
+                    </div>
+                </div>)
             }
             
-                    
-                
+            {toggleForm ? (
+                <div>
+                    <h2>Preencha os dados abaixo</h2>
+                    <label>Email: </label>
+                    <input className='campoEmail' type='email' onChange={(ev) => setEmail(ev.target.value)}></input>
+                            
+                    <label>Senha: </label>
+                    <input className='campoSenha' type='password' onChange={(ev) => setPassword(ev.target.value)}></input>
 
-                
-            
-            
-            {toggleForm ? <Form /> : null}
-           </div> 
-        
+                    <button className="botao" onClick={() => dispatch(register(email, password))}>Cadastrar</button>
+                </div>
+            ) : null}
+        </div>
     );
 }
 

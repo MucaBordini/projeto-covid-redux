a88a2f9428997d6b1afb80f11ea5b331b91fba00
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-04 22:10:34 -0300

    Criando ação para registrar usuário na api

--- src/features/header/headerSlice.js
+++ src/features/header/headerSlice.js
@@ -5,15 +5,15 @@ export const headerSlice = createSlice({
     name: 'user',
     initialState: { token: ''},
     reducers: {
-        set_login: (state, action) => {
-          state.token = action.payload;
-          return state;
-        },
-        set_register: (state, action) => {
-          state.token = action.payload;
-          return state;
-        }
+      set_login: (state, action) => {
+        state.token = action.payload;
+        return state;
+      },
+      set_register: (state, action) => {
+        state.token = action.payload;
+        return state;
       }
+    }
 });
 
 export const user_map = (state) => state.user.token;
@@ -27,8 +27,19 @@ export const login = (emailUser, passwordUser) => async (dispatch) => {
       console.log(response.data);
       dispatch(headerActions.set_login(response.data));
     });
-  
-    
-  }
+}
+
+export const register = (emailUser, passwordUser) => async (dispatch) => {
+  await apiLogin.post('users', {
+    email: emailUser,
+    password: passwordUser
+  }).then((response) => {
+    console.log(response.data);
+    alert('Usuário criado com sucesso!');
+    dispatch(headerActions.set_register(response.data));
+  }).catch( error => {
+    alert('Erro ao criar novo usuário\n', error);
+  });
+}
 
 export default headerSlice.reducer;

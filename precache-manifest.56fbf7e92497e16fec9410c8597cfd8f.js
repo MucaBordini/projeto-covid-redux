871a208ad8d29dfb429a05763cfd0cdeb6a7cd64
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-08 17:21:13 -0300

    Updates

---
+++ precache-manifest.56fbf7e92497e16fec9410c8597cfd8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77766c2eb42243399dc207bd9f5427a",
+    "url": "/projeto-covid-redux/index.html"
+  },
+  {
+    "revision": "c2d0f5e4e75f2e50c4ae",
+    "url": "/projeto-covid-redux/static/css/main.64b78760.chunk.css"
+  },
+  {
+    "revision": "6e359440aaab93cce900",
+    "url": "/projeto-covid-redux/static/js/2.88b7e53e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projeto-covid-redux/static/js/2.88b7e53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d0f5e4e75f2e50c4ae",
+    "url": "/projeto-covid-redux/static/js/main.26bd9d3b.chunk.js"
+  },
+  {
+    "revision": "22e29e02c074937cba66",
+    "url": "/projeto-covid-redux/static/js/runtime-main.ab82aff3.js"
+  },
+  {
+    "revision": "21fabd9c25168b2d44ab74438d6499ea",
+    "url": "/projeto-covid-redux/static/media/coronavirus.21fabd9c.png"
+  }
+]);
